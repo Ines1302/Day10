bc5fa70f3ccaa7ea15de0f10b92464c23161928c
Ines1302 <seeking.knowledge13@gmail.com>
2021-03-17 11:26:07 +0100

    Create index.js

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+var myPet = {
+    species: "Dog" ,
+    name: "Lusy" ,
+    legs: 4 ,
+    friends: ['Jerry', 'Tom']
+}
+
+function myFunction(myObj){
+    return myObj;
+}
+
+console.log(myFunction(myPet));
+module.exports = { myPet, myFunction };
